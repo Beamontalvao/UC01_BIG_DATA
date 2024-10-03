6078a551b55efa726429916acd3531816f116e8c
Beamontalvao <beamontalvao21@gmail.com>
2024-10-02 21:29:57 -0300

    Add files via upload

---
+++ AULA_3/Exemplo_1.py
@@ -0,0 +1,9 @@
+idade = int(input("Informe a Idade: "))
+if idade < 18:
+     print("Você é Menor de Idade")
+elif idade == 18:
+      print("Quase lá")
+elif idade > 65:
+     print("Acesso Liberado - Aprecie com Moderação")
+else:
+    print("Acesso Liberado")
---
+++ AULA_3/Exemplo_4.py
@@ -0,0 +1,6 @@
+n1 = int(input("Digite o primeiro Valor Inteiro: "))
+n2 = int(input("Digite o segundo Valor Inteiro: "))
+if n1 == n2:
+   print("Os Valores são Iguais")
+else:
+   print("Os Valores são Diferentes")
